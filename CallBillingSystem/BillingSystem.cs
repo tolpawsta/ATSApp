7542b8a36719e5339c90f08e5ac5af9a0bd18f76
Pavel <tolpawsta@gmail.com>
2019-12-11 18:41:27 +0300

    add projects for solution
    add  ATSDemo, ATS, Terminal, CallBillingSystem, ATSCore, ATS
    add referenses form ATSCore to another projects

---
+++ CallBillingSystem/BillingSystem.cs
@@ -0,0 +1,13 @@
+﻿using ATSCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallBillingSystem
+{
+    public class BillingSystem:IBillingable
+    {
+    }
+}
